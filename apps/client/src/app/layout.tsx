c44fc4e404aae2c5753297441741e0fa7cb2f5b5
DevDesignAmitesh <amiteshsingh252@gmail.com>
2026-02-03 13:49:34 +0530

    added google analytics

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,9 @@ export default function RootLayout({
       >
         {children}
         <Toaster position="top-right" richColors />
+
+        {/* Google Analytics) */}
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
       </body>
     </html>
   );
