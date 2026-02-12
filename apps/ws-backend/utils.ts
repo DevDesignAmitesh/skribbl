5e5af85fb2a49bbe9ffa9afcde4501cb98ba92cc
DevDesignAmitesh <amiteshsingh252@gmail.com>
2026-02-12 15:44:25 +0530

    added more words

--- apps/ws-backend/utils.ts
+++ apps/ws-backend/utils.ts
@@ -2,4 +2,16 @@ export const random_words = [
   "cat, dog, house, car, sun, ninja, robot, dragon, airplane, skateboard, volcano",
   "apple, ball, hat, book, chair, phone, pirate, wizard, unicorn, ghost, lighthouse",
   "tree, fish, cloud, cake, cup, key, dinosaur, alien, superhero, snowman, waterfall",
+  "pizza, guitar, crown, butterfly, hammer, rocket, mermaid, castle, rainbow, spider, mountain",
+  "flower, shoe, clock, banana, lamp, sword, vampire, fairy, treasure, octopus, bridge",
+  "ice cream, bicycle, glasses, moon, bottle, shield, knight, phoenix, diamond, shark, tower",
+  "sandwich, drum, mask, star, pencil, bow, angel, monster, crystal, whale, pyramid",
+  "cookie, violin, helmet, heart, scissors, arrow, witch, giant, emerald, dolphin, temple",
+  "burger, piano, glove, lightning, brush, spear, demon, elf, ruby, eagle, fortress",
+  "donut, trumpet, boot, fire, ruler, axe, zombie, troll, sapphire, lion, cathedral",
+  "taco, flute, scarf, wind, eraser, dagger, ghost, goblin, pearl, tiger, palace",
+  "hotdog, harp, tie, rain, stapler, club, spirit, dwarf, gold, bear, mansion",
+  "cupcake, banjo, sock, snow, calculator, mace, phantom, orc, silver, wolf, villa",
+  "waffle, accordion, belt, storm, compass, lance, wraith, gnome, bronze, fox, cottage",
+  "pancake, harmonica, jacket, thunder, telescope, trident, specter, pixie, copper, deer, cabin"
 ];
