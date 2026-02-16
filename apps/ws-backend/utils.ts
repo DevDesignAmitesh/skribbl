e8473f270998d2278c1f73596872f3a3501d99b8
DevDesignAmitesh <amiteshsingh252@gmail.com>
2026-02-16 10:42:32 +0530

    fixed random words

--- apps/ws-backend/utils.ts
+++ apps/ws-backend/utils.ts
@@ -1,17 +1,12 @@
 export const random_words = [
-  "cat, dog, house, car, sun, ninja, robot, dragon, airplane, skateboard, volcano",
-  "apple, ball, hat, book, chair, phone, pirate, wizard, unicorn, ghost, lighthouse",
-  "tree, fish, cloud, cake, cup, key, dinosaur, alien, superhero, snowman, waterfall",
-  "pizza, guitar, crown, butterfly, hammer, rocket, mermaid, castle, rainbow, spider, mountain",
-  "flower, shoe, clock, banana, lamp, sword, vampire, fairy, treasure, octopus, bridge",
-  "ice cream, bicycle, glasses, moon, bottle, shield, knight, phoenix, diamond, shark, tower",
-  "sandwich, drum, mask, star, pencil, bow, angel, monster, crystal, whale, pyramid",
-  "cookie, violin, helmet, heart, scissors, arrow, witch, giant, emerald, dolphin, temple",
-  "burger, piano, glove, lightning, brush, spear, demon, elf, ruby, eagle, fortress",
-  "donut, trumpet, boot, fire, ruler, axe, zombie, troll, sapphire, lion, cathedral",
-  "taco, flute, scarf, wind, eraser, dagger, ghost, goblin, pearl, tiger, palace",
-  "hotdog, harp, tie, rain, stapler, club, spirit, dwarf, gold, bear, mansion",
-  "cupcake, banjo, sock, snow, calculator, mace, phantom, orc, silver, wolf, villa",
-  "waffle, accordion, belt, storm, compass, lance, wraith, gnome, bronze, fox, cottage",
-  "pancake, harmonica, jacket, thunder, telescope, trident, specter, pixie, copper, deer, cabin"
+  "cat, dog, house, car, sun, tree, fish, cloud, cake, cup",
+  "apple, ball, hat, book, chair, phone, flower, shoe, clock, banana",
+  "pizza, guitar, crown, butterfly, hammer, rocket, star, heart, scissors, moon",
+  "ice cream, bicycle, glasses, bottle, drum, mask, pencil, sandwich, cookie, burger",
+  "donut, trumpet, boot, fire, ruler, snowman, rainbow, spider, mountain, bridge",
+  "taco, scarf, rain, brush, shark, whale, lion, tiger, bear, fox",
+  "airplane, skateboard, volcano, dinosaur, alien, superhero, ghost, robot, ninja, pirate",
+  "lamp, key, sword, shield, bow, arrow, glove, helmet, brush, balloon",
+  "pancake, waffle, cupcake, violin, piano, camera, backpack, ladder, train, bus",
+  "bed, sofa, table, door, window, plate, spoon, fork, toothbrush, mirror"
 ];
